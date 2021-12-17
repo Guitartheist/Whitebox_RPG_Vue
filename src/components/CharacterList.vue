5ea1213f186eac880d0e5122eeec9ea33a7cac84
Philip Wentworth <guitartheist@gmail.com>
2021-12-16 20:13:35 -0500

    Created character list component to pull a list of character data from Django REST endpoint

---
+++ src/components/CharacterList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <ol>
+    <li v-for="character in characters" v-bind:key="character">
+      {{ character.name }}
+    </li>
+  </ol>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      characters: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios.get(`http://127.0.0.1:8000/whitebox/character_list`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.characters = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
